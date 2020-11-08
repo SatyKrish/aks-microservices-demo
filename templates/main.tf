2ac58642bb121263f821504bd185e0d30155f2d7
SatyKrish <sathishkumar.krishnan@hotmail.com>
2020-11-08 22:23:14 +0530

    Updated user node pool to use standard vm (#18)
    * Updated user node pool to use standard vm
    
    * fixed format errors

--- templates/main.tf
+++ templates/main.tf
@@ -78,7 +78,6 @@ resource "azurerm_kubernetes_cluster" "k8s_cluster" {
     load_balancer_sku = "standard"
   }
 
-
   identity {
     type = "SystemAssigned"
   }
@@ -122,15 +121,15 @@ resource "azurerm_kubernetes_cluster_node_pool" "k8s_nodepool_dev" {
   mode                  = "User"
   enable_auto_scaling   = true
   min_count             = 1
-  max_count             = 3
+  max_count             = 10
   availability_zones    = [1, 2, 3]
-  priority              = "Spot"
-  eviction_policy       = "Delete"
-  spot_max_price        = 0.5
+  # priority              = "Spot"
+  # eviction_policy       = "Delete"
+  # spot_max_price        = 0.5
 
-  node_labels = {
-    "kubernetes.azure.com/scalesetpriority" = "spot"
-  }
+  # node_labels = {
+  #   "kubernetes.azure.com/scalesetpriority" = "spot"
+  # }
 
   node_taints = [
     "kubernetes.azure.com/scalesetpriority=spot:NoSchedule"
